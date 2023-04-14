34b42eb95e1e647ac9d80f44839ec420f4eaa68f
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-14 01:09:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -55,12 +55,14 @@ def telegram_bot():
     date = datetime.fromtimestamp(update['message']['date']).date()
     time = datetime.fromtimestamp(update['message']['date']).time()
 
+    resposta_wiki = wikifuncao.wikifuncao(message)
+    
 # Define resposta
     texto_resposta = ""
     if message == "/start":
         texto_resposta = "Olá! Seja bem-vinda(o). Esse bot te dá algumas análises sobre a página da Wikipédia que você escolher. Na próxima mensagem, envie o título da página (verbete) que deseja acompanhar:"
     else:
-        texto_resposta = WikiFuncao.WikiFuncao(message)
+        texto_resposta = resposta_wiki
     nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
     requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
 
