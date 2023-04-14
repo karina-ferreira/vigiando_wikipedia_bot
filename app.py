8c32891cd7d3193fdb63c8d164c84162873dbc17
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-14 04:38:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,36 +31,42 @@ planilha_recebidas = planilha.worksheet("mensagens_recebidas")
   
 # ______________________________[site]__________________________________________
 
+# Variável global para armazenar o update_id da última mensagem processada
+last_update_id = 0
+
 @app.route("/telegram-bot", methods = ["POST"])
 def telegram_bot():
-  
+    global last_update_id
+
     update = request.json 
 
-# Dados da mensagem
-    update_id = update['update_id']
-    first_name = update['message']['from']['first_name']
-    last_name = update['message']['from']['last_name']
-    user_name = update['message']['from']['username']
-    sender_id = update['message']['from']['id']
-    chat_id = update['message']['chat']['id']
-    message = update["message"]["text"]
-    date = datetime.fromtimestamp(update['message']['date']).date()
-    time = datetime.fromtimestamp(update['message']['date']).time()
+    # Verifica se há uma nova mensagem a ser processada
+    if update["update_id"] > last_update_id:
+        last_update_id = update["update_id"]
 
-    #resposta_wiki = wikifuncao.wikifuncao(message)
-    
-# Define resposta
-    if message == "/start":
-        texto_resposta = "Olá! Seja bem-vinda(o). Esse bot te dá algumas análises sobre a página da Wikipédia que você escolher. Na próxima mensagem, envie o título da página (verbete) que deseja acompanhar:"
-    else:
-        texto_resposta = "entrou na segunda condição" #resposta_wiki
+        # Dados da mensagem
+        first_name = update['message']['from']['first_name']
+        last_name = update['message']['from']['last_name']
+        user_name = update['message']['from']['username']
+        sender_id = update['message']['from']['id']
+        chat_id = update['message']['chat']['id']
+        message = update["message"]["text"]
+        date = datetime.fromtimestamp(update['message']['date']).date()
+        time = datetime.fromtimestamp(update['message']['date']).time()
+
+        #resposta_wiki = wikifuncao.wikifuncao(message)
         
-# Envia a resposta        
-    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta} 
-    requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+        # Define resposta
+        if message == "/start":
+            texto_resposta = "Olá! Seja bem-vinda(o). Esse bot te dá algumas análises sobre a página da Wikipédia que você escolher. Na próxima mensagem, envie o título da página (verbete) que deseja acompanhar:"
+        else:
+            texto_resposta = "entrou na segunda condição" #resposta_wiki
+            
+        # Envia a resposta        
+        nova_mensagem = {"chat_id": chat_id, "text": texto_resposta} 
+        requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
 
-# Atualiza planilha do sheets com último update processado
-   
-    #gravar_sheets.gravar_sheets(nova_mensagem)
+        # Atualiza planilha do sheets com último update processado
+        #gravar_sheets.gravar_sheets(nova_mensagem)
 
     return "ok"
